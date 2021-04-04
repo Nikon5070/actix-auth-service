50642a2182a448ea5e6bb9577dda564814b5eacd
Nikita Kuprin <nikon5070@gmail.com>
2021-04-05 01:21:47 +1000

    add tracing and config from env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use actix_web::{middleware::Logger, App, HttpServer};
+use color_eyre::Result;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let config = config::Config::from_env().expect("error loading .env");
+
+    HttpServer::new(|| App::new().wrap(Logger::default()))
+        .bind(format!("{}:{}", config.host, config.port))?
+        .run()
+        .await?;
+
+    Ok(())
 }
