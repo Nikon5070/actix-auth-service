fa12c43affb0fadb3e4e7989c7592dc84285f68b
Nikita Kuprin <nikon5070@gmail.com>
2021-04-05 01:26:18 +1000

    add info starting server

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod config;
 
 use actix_web::{middleware::Logger, App, HttpServer};
 use color_eyre::Result;
+use tracing::info;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     let config = config::Config::from_env().expect("error loading .env");
+
+    info!("Starting server at http://{}:{}", config.host, config.port);
 
     HttpServer::new(|| App::new().wrap(Logger::default()))
         .bind(format!("{}:{}", config.host, config.port))?
